abcad6bcd10f5587ea676751123800f6a0859585
Ali A Eftekhari <e.eftekhari@gmail.com>
2021-06-08 16:05:59 +0200

    reviving IPhreeqc.jl
    database, build functions, clean up

---
+++ deps/build.jl
@@ -0,0 +1,35 @@
+if Sys.iswindows()
+    url = "https://github.com/simulkade/PhreeqcRM/releases/download/3.7/IPhreeqcd.dll"
+    lib_path = joinpath(@__DIR__, "..", "lib", "libiphreeqc.dll")
+    if isfile(lib_path)
+        println("libiphreeqc.dll exists. Skiping download ...")
+    else
+        try
+            println("Downloading the library files ...")
+            download(url, joinpath(@__DIR__, "..", "lib", "libiphreeqc.dll"))
+            println("PhreeqcRM.dll downloaded successfully.")
+        catch
+            @warn "Could not downlaod the library file PhreeqcRM.dll"
+            @warn "Download it from: $url"
+            @warn "and copy it into the IPhreeqc/lib folder"
+        end
+    end
+elseif Sys.islinux()
+    lib_path = joinpath(@__DIR__, "..", "lib", "libphreeqcrm.so")
+    url = "https://github.com/simulkade/PhreeqcRM/releases/download/3.7/libiphreeqc-3.7.0.so"
+    if isfile(lib_path)
+        println("PhreeqcRM.dll exists. Skiping download ...")
+    else
+        try
+            println("Downloading the library files ...")
+            download(url, joinpath(@__DIR__, "..", "lib", "libiphreeqc.so"))
+            println("libphreeqcrm.so downloaded successfully.")
+        catch
+            @warn "Could not downlaod the library file libiphreeqc.so"
+            @warn "Download it from: $url"
+            @warn "and copy it into the IPhreeqc/lib folder"
+        end
+    end
+else
+    @error "No binary files for your OS. Only Windows and Linux at the moment!"
+end
